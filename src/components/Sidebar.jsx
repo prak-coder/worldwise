d7d32161b2355e489dcb84e563f06fd15bbca79b
Prakash K <prakash.change@gmail.com>
2024-01-18 10:55:55 +0530

    building app layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import Footer from "./Footer";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of Cities</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
